26cc659cbf9684591dff3b1745155b109d5fbc64
Igor Tamashchuk <legigor@gmail.com>
2015-09-10 16:22:26 +0300

    Init client project

---
+++ Talkach/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Orleans;
+
+namespace Talkach
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("-= Talkach - the chat app for those who respect the Actors Model =-\n\n");
+
+            Console.Write("Your nickname: ");
+            var name = Console.ReadLine();
+
+            Console.WriteLine($"Joining the chat as {name} . . .");
+
+            GrainClient.Initialize("ClientConfiguration.xml");
+        }
+    }
+}
